b00ac51b53c6f826e5d1c7456fbd0b4947f915b4
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-03-31 16:07:08 +0530

    Update donorform.js

--- backend/routes/donorform.js
+++ backend/routes/donorform.js
@@ -8,7 +8,7 @@ const router = express.Router(); // FIXED: use express.Router()
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 25, // Use 587 instead of 465
+  port: 587, // Use 587 instead of 465
   secure: false, // false for STARTTLS (recommended)
   auth: {
     user: process.env.EMAIL_USER,
