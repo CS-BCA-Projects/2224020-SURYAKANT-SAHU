d739be4f45437c28eddc8f003952ae8c523f26c5
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-03-31 15:55:54 +0530

    Update adminForgot_Routes.js

--- backend/routes/adminForgot_Routes.js
+++ backend/routes/adminForgot_Routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
+    port: 25,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
