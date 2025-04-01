9cdd5ff14408ee5eee241f6f6d467d9287e176ec
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-04-01 14:08:28 +0530

    Update adminSignup_Routes.js

--- backend/routes/adminSignup_Routes.js
+++ backend/routes/adminSignup_Routes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,// Use 587 instead of 465
-  secure: true, // false for STARTTLS (recommended)
+  secure: false, // false for STARTTLS (recommended)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
