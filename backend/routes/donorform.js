3dffa4e357b06355a4932ed82b146cd0cb633bbb
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-04-02 13:57:32 +0530

    Update donorform.js

--- backend/routes/donorform.js
+++ backend/routes/donorform.js
@@ -52,7 +52,7 @@ router.post("/donorform", authenticateUser, async (req, res) => {
     } = req.body;
 
     // Check if donor already exists
-    const existingDonor = await Donorregister.findOne({ userId: req.user.userId });
+    const existingDonor = await Donorregister.findById({ userId: req.user.userId });
     if (existingDonor) {
       return res.status(400).json({ msg: "You have already registered" });
     }
