446d7297589f4c8642b5fb6574b8224c7c889d25
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-04-04 13:47:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -110,7 +110,7 @@ app.get("/donor/edit", (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
 });
