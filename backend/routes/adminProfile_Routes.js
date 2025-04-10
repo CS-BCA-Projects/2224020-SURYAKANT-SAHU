c8823a9b064ca2b4013dae9d17c06564d6b908b8
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-04-10 14:50:26 +0530

    Update adminProfile_Routes.js

--- backend/routes/adminProfile_Routes.js
+++ backend/routes/adminProfile_Routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { authenticateUser } from "../middlewares/authMiddleware.js";
 import Admin from "../models/admin_signup.js";
 import Donorregister from "../models/donorregisterform.js";
+import sendEmail from '../utils/send_mail.js';
 
 const router = express.Router();
 
@@ -38,7 +39,12 @@ router.patch('/approve/:userId', async (req, res) => {
         if (!updated) {
             return res.status(404).json({ error: "Donor not found" });
         }
-
+      const emailSent = await sendEmail (
+      email,
+      "Registeration successfully ",
+      `<p>You are successfully registered for blood donation you are approved for blood donation by bloodlink 
+      thankyou </p>`
+    );
         res.json({ message: "Donor approved successfully", donor: updated });
     } catch (err) {
         console.error(err);
@@ -57,6 +63,13 @@ router.delete('/reject/:userId', async (req, res) => {
         }
 
         res.json({ message: "Donor rejected and removed" });
+      const emailSent = await sendEmail (
+      email,
+      "Registeration rejected",
+      `<p>You are Rejected for blood donation you are not approved for blood donation by bloodlink.
+      please input correct information thankyou
+      </p>`
+    );
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Rejection failed" });
