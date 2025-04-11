954db4e2c6eadf67bd5bfa678b120792a74a65dc
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-04-11 23:29:16 +0530

    Update profileRoutes.js

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { authenticateUser } from "../middlewares/authMiddleware.js";
 import Donor from "../models/user_signup.js";
+import Request from "../models/request .js";
 import Donorregister from "../models/donorregisterform.js"; // Import Donorregister model
 
 const router = express.Router();
@@ -29,5 +30,14 @@ router.get("/profile", authenticateUser, async (req, res) => {
         res.status(500).json({ message: "❌ Server error" });
     }
 });
-
+router.get("/request", authenticateUser, async (req, res) =>{
+    try{
+       const requester = await Request.find({requestedTo: req.user.userId }); 
+        console.log(requester)
+    }
+    catch(error){
+         console.error(error);
+        res.status(500).json({ message: "❌ Server error" });
+    }
+})
 export default router;
