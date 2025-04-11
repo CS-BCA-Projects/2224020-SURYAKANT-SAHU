a633e9bcdce5c3799f2f71fa4387f87fe066707a
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-04-11 14:22:11 +0530

    Update request.js

--- backend/models/request.js
+++ backend/models/request.js
@@ -1,1 +1,10 @@
+import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  requestedTo:{ type: mongoose.Schema.Types.ObjectId, ref: "Donor", required: true },
+  contactNo: { type: String, required: true , sparse: true },
+});
+const Request = mongoose.model("request", UserSchema);
+export default Request
