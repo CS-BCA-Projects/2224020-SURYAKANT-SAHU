02ea23202e69489e684f5f0efe821c161b6e48d8
Suryakant sahu <133488558+Suryakant7879@users.noreply.github.com>
2025-04-12 00:38:29 +0530

    Update profileRoutes.js

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -3,6 +3,7 @@ import { authenticateUser } from "../middlewares/authMiddleware.js";
 import Donor from "../models/user_signup.js";
 import Request from "../models/request.js";
 import Donorregister from "../models/donorregisterform.js"; // Import Donorregister model
+import sendEmail from '../utils/send_mail.js';
 
 const router = express.Router();
 
@@ -41,4 +42,77 @@ router.get("/request", authenticateUser, async (req, res) =>{
         res.status(500).json({ message: "❌ Server error" });
     }
 })
+
+router.post("/:id/accept", async (req, res) => {
+  try {
+    const request = await Request.findById(req.params.id);
+
+    if (!request) return res.status(404).json({ message: "Request not found" });
+
+    const donor =await Donorregister.findOne({userId: request.requestedTo }); ;
+
+     const emailSent = await sendEmail (
+      request.email,
+      "Blood Request Accepted",
+      `Dear ${request.name},
+
+        Your blood request has been accepted!
+        
+        Here are the donor's details:
+        - Name: ${donor.name}
+        - Email: ${donor.email}
+        - Contact No: ${donor.contactNo}
+        - Address: ${donor.address || "Not Provided"}
+        
+        Please contact the donor as soon as possible.
+        
+        Thank you,
+        Bloodlink.com
+              `,`
+            );
+
+    // Optional: Delete the request after handling
+    await Request.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "Request accepted and email sent." });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error processing acceptance." });
+  }
+});
+
+// ❌ REJECT REQUEST
+router.post("/:id/reject", async (req, res) => {
+  try {
+    const request = await Request.findById(req.params.id);
+
+    if (!request) return res.status(404).json({ message: "Request not found" });
+
+    const donor =await Donorregister.findOne({userId: request.requestedTo }); ;
+
+    const emailSent = await sendEmail (
+      request.email,
+      "Blood Request rejected",
+      `Dear ${request.name},
+
+We regret to inform you that your blood request has been rejected by the $(donor.First_Name) $(donor.Last_Name) .
+
+You may try contacting other donors through the system.
+
+Regards,
+Blood Donation System
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    // Optional: Delete the request after handling
+    await Request.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "Request rejected and email sent." });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error processing rejection." });
+  }
+});
 export default router;
